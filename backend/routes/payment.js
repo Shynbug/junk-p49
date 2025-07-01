8d38241ba6b52820c1938146b54898cc950daccd
Kian Moldenhauer <kian.moldenhauer@gmail.com>
2025-07-01 17:22:07 +0200

    Create payment.js

---
+++ backend/routes/payment.js
@@ -0,0 +1,10 @@
+// routes/auth.js (or test.js or payment.js)
+const express = require('express');
+const router = express.Router();
+
+// Define routes
+router.get('/', (req, res) => {
+  res.send('This is the payment route');
+});
+
+module.exports = router; // ✅ Export the router
